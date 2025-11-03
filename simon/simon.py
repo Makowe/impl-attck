048c42f89098e253179d262cbbf70d2a1d6e0ba1
Makowe <nico.makowe01@gmail.com>
2025-11-03 17:36:13 +0100

    fix simon python implementation

--- simon/simon.py
+++ simon/simon.py
@@ -30,6 +30,9 @@ class Simon:
         self.key_size_bytes = (n * m) // 8
         """Key size in bytes."""
 
+        self.mask = ~np.uint64(0) >> (64 - n)
+        """ Mask to apply on uint64 to keep only n bits."""
+
 
 SIMON_32_64 = Simon(16, 4, 0, 32)
 SIMON_48_72 = Simon(24, 3, 0, 36)
@@ -63,13 +66,11 @@ def encrypt_block(algo: Simon, plaintext: np.ndarray, key: np.ndarray) -> np.nda
     assert key.dtype == np.uint64
     assert plaintext.shape == (2,)
     assert key.shape == (algo.m,)
-    plaintext = clean_input(plaintext, algo.n)
-    key = clean_input(key, algo.n)
+    plaintext = clean_input(plaintext, algo)
+    key = clean_input(key, algo)
 
-    # Expand the key.
+    # Prepare buffers
     round_keys = expand_key(algo, key)
-
-    # Split plaintext into left and right words.
     x = plaintext[0]
     y = plaintext[1]
 
@@ -78,8 +79,8 @@ def encrypt_block(algo: Simon, plaintext: np.ndarray, key: np.ndarray) -> np.nda
         tmp = x
         x = (
             y
-            ^ (rotate_left(x, 1, algo.n) & rotate_left(x, 8, algo.n))
-            ^ rotate_left(x, 2, algo.n)
+            ^ (rotate_left(x, 1, algo) & rotate_left(x, 8, algo))
+            ^ rotate_left(x, 2, algo)
             ^ round_keys[i]
         )
         y = tmp
@@ -87,49 +88,70 @@ def encrypt_block(algo: Simon, plaintext: np.ndarray, key: np.ndarray) -> np.nda
     return np.array([x, y], dtype=np.uint64)
 
 
-def clean_input(input: np.ndarray, n: int) -> np.ndarray:
+def clean_input(input: np.ndarray, algo: Simon) -> np.ndarray:
     """Clean an array of words to ensure each only has n bits."""
-    mask = (np.uint64(1) << n) - 1  # mask to keep only n least significant bits.
-    return input & mask
+    return input & algo.mask
 
 
 def expand_key(algo: Simon, key: np.ndarray) -> np.ndarray:
 
     # Result array will have m keys, each m*n bits long.
     k = np.zeros((algo.t,), dtype=np.uint64)
-    k[0 : algo.m] = key
-
-    # Constant c = 2^n - 4
-    c = (np.uint64(1) << algo.n) - 4
+    for i in range(algo.m):
+        k[i] = key[algo.m - i - 1]
 
     for i in range(algo.m, algo.t):
-        tmp = rotate_left(k[i - 1], -3, algo.n)
+        tmp = rotate_left(k[i - 1], -3, algo)
         if algo.m == 4:
             tmp ^= k[i - 3]
-        tmp ^= rotate_left(tmp, -1, algo.n)
+        tmp ^= rotate_left(tmp, -1, algo)
 
         z_i = get_round_constant(algo.j, i - algo.m)
-        k[i] = invert(k[i - algo.m], algo.n) ^ tmp ^ z_i ^ 3
+        k[i] = invert(k[i - algo.m], algo) ^ tmp ^ z_i ^ 3
 
     return k
 
 
-def invert(word: np.uint64, n: int) -> np.uint64:
+def expand_key_alt(algo: Simon, key: np.ndarray) -> np.ndarray:
+    # Result array will have m keys, each m*n bits long.
+    k = np.zeros((algo.t,), dtype=np.uint64)
+    for i in range(algo.m):
+        k[i] = key[algo.m - i - 1]
+
+    # Constant c = 2^n - 4
+    c = (np.uint64(1) << algo.n) - 4
+
+    for i in range(0, algo.t - algo.m):
+        z_i = get_round_constant(algo.j, i)
+        if algo.m == 2:
+            tmp1 = rotate_left(k[i + 1], -3, algo)
+            tmp2 = rotate_left(tmp1, -1, algo)
+            k[i + algo.m] = c ^ z_i ^ k[i] ^ tmp1 ^ tmp2
+        if algo.m == 3:
+            tmp1 = rotate_left(k[i + 2], -3, algo)
+            tmp2 = rotate_left(tmp1, -1, algo)
+            k[i + algo.m] = c ^ z_i ^ k[i] ^ tmp1 ^ tmp2
+        if algo.m == 4:
+            tmp1 = rotate_left(k[i + 3], -3, algo) ^ k[i + 1]
+            tmp2 = rotate_left(tmp1, -1, algo)
+            k[i + algo.m] = c ^ z_i ^ k[i] ^ tmp1 ^ tmp2
+    return k
+
+
+def invert(word: np.uint64, algo: Simon) -> np.uint64:
     """Invert a word of n bits."""
-    mask: np.uint64 = np.left_shift(1, n, dtype=np.uint64) - 1
-    return (~word) & mask
+    if algo.n == 64:
+        return ~word
+    return (~word) & algo.mask
 
 
-def rotate_left(word: np.uint64, bits: int, n: int) -> np.uint64:
+def rotate_left(word: np.uint64, bits: int, algo: Simon) -> np.uint64:
     """Rotate a word left."""
-    mask: np.uint64 = np.left_shift(1, n, dtype=np.uint64) - 1
-    np.uint64((1 << n) - 1)  # mask to keep only n least significant bits.
-
     if bits >= 0:
-        return (word << bits | word >> (n - bits)) & mask
+        return (word << bits | word >> (algo.n - bits)) & algo.mask
     else:
         bits = -bits
-        return (word >> bits | word << (n - bits)) & mask
+        return (word >> bits | word << (algo.n - bits)) & algo.mask
 
 
 def get_round_constant(j: int, round: int) -> np.uint64:
--- simon/test_simon.py
+++ simon/test_simon.py
@@ -6,6 +6,30 @@ import simon
 
 
 class TestSimon(unittest.TestCase):
+    def test_expand_key_alt(self):
+        """No test vectors are available for key expansion, so checking two alternative implementations
+        give the same result. Additionally, the first implementation is implicitly tested through the encryption tests.
+        """
+        for algo in [
+            simon.SIMON_32_64,
+            simon.SIMON_48_72,
+            simon.SIMON_48_96,
+            simon.SIMON_64_96,
+            simon.SIMON_64_128,
+            simon.SIMON_96_96,
+            simon.SIMON_96_144,
+            simon.SIMON_128_128,
+            simon.SIMON_128_192,
+            simon.SIMON_128_256,
+        ]:
+            for _ in range(10):
+                key = np.random.randint(0, 2**algo.n, size=(algo.m,), dtype=np.uint64)
+
+                expanded_key = simon.expand_key(algo, key)
+                expanded_key_alt = simon.expand_key_alt(algo, key)
+
+                np.testing.assert_array_equal(expanded_key, expanded_key_alt)
+
     def test_invert(self):
         word = np.uint64(0b1010_1100_1111_0000)
 
@@ -101,11 +125,107 @@ class TestSimon(unittest.TestCase):
         )
         self.assertEqual(rotated_right_4, expected_right_4)
 
-    def test_encrypt_simon_32_64(self):
+    def test_simon_32_64(self):
         algo = simon.SIMON_32_64
         key = np.array([0x1918, 0x1110, 0x0908, 0x0100], dtype=np.uint64)
         plaintext = np.array([0x6565, 0x6877], dtype=np.uint64)
         expected_ciphertext = np.array([0xC69B, 0xE9BB], dtype=np.uint64)
+        ciphertext = simon.encrypt_block(algo, plaintext, key)
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext)
 
+    def test_simon_48_72(self):
+        algo = simon.SIMON_48_72
+        key = np.array([0x121110, 0x0A0908, 0x020100], dtype=np.uint64)
+        plaintext = np.array([0x612067, 0x6E696C], dtype=np.uint64)
+        expected_ciphertext = np.array([0xDAE5AC, 0x292CAC], dtype=np.uint64)
+        ciphertext = simon.encrypt_block(algo, plaintext, key)
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext)
+
+    def test_simon_48_96(self):
+        algo = simon.SIMON_48_96
+        key = np.array([0x1A1918, 0x121110, 0x0A0908, 0x020100], dtype=np.uint64)
+        plaintext = np.array([0x726963, 0x20646E], dtype=np.uint64)
+        expected_ciphertext = np.array([0x6E06A5, 0xACF156], dtype=np.uint64)
+        ciphertext = simon.encrypt_block(algo, plaintext, key)
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext)
+
+    def test_simon_64_96(self):
+        algo = simon.SIMON_64_96
+        key = np.array([0x13121110, 0x0B0A0908, 0x03020100], dtype=np.uint64)
+        plaintext = np.array([0x6F722067, 0x6E696C63], dtype=np.uint64)
+        expected_ciphertext = np.array([0x5CA2E27F, 0x111A8FC8], dtype=np.uint64)
+        ciphertext = simon.encrypt_block(algo, plaintext, key)
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext)
+
+    def test_simon_64_128(self):
+        algo = simon.SIMON_64_128
+        key = np.array(
+            [0x1B1A1918, 0x13121110, 0x0B0A0908, 0x03020100], dtype=np.uint64
+        )
+        plaintext = np.array([0x656B696C, 0x20646E75], dtype=np.uint64)
+        expected_ciphertext = np.array([0x44C8FC20, 0xB9DFA07A], dtype=np.uint64)
+        ciphertext = simon.encrypt_block(algo, plaintext, key)
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext)
+
+    def test_simon_96_96(self):
+        algo = simon.SIMON_96_96
+        key = np.array([0x0D0C0B0A0908, 0x050403020100], dtype=np.uint64)
+        plaintext = np.array([0x2072616C6C69, 0x702065687420], dtype=np.uint64)
+        expected_ciphertext = np.array(
+            [0x602807A462B4, 0x69063D8FF082], dtype=np.uint64
+        )
+        ciphertext = simon.encrypt_block(algo, plaintext, key)
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext)
+
+    def test_simon_96_144(self):
+        algo = simon.SIMON_96_144
+        key = np.array(
+            [0x151413121110, 0x0D0C0B0A0908, 0x050403020100], dtype=np.uint64
+        )
+        plaintext = np.array([0x746168742074, 0x73756420666F], dtype=np.uint64)
+        expected_ciphertext = np.array(
+            [0xECAD1C6C451E, 0x3F59C5DB1AE9], dtype=np.uint64
+        )
+        ciphertext = simon.encrypt_block(algo, plaintext, key)
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext)
+
+    def test_simon_128_128(self):
+        algo = simon.SIMON_128_128
+        key = np.array([0x0F0E0D0C0B0A0908, 0x0706050403020100], dtype=np.uint64)
+        plaintext = np.array([0x6373656420737265, 0x6C6C657661727420], dtype=np.uint64)
+        expected_ciphertext = np.array(
+            [0x49681B1E1E54FE3F, 0x65AA832AF84E0BBC], dtype=np.uint64
+        )
+        ciphertext = simon.encrypt_block(algo, plaintext, key)
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext)
+
+    def test_simon_128_192(self):
+        algo = simon.SIMON_128_192
+        key = np.array(
+            [0x1716151413121110, 0x0F0E0D0C0B0A0908, 0x0706050403020100],
+            dtype=np.uint64,
+        )
+        plaintext = np.array([0x206572656874206E, 0x6568772065626972], dtype=np.uint64)
+        expected_ciphertext = np.array(
+            [0xC4AC61EFFCDC0D4F, 0x6C9C8D6E2597B85B], dtype=np.uint64
+        )
+        ciphertext = simon.encrypt_block(algo, plaintext, key)
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext)
+
+    def test_simon_128_256(self):
+        algo = simon.SIMON_128_256
+        key = np.array(
+            [
+                0x1F1E1D1C1B1A1918,
+                0x1716151413121110,
+                0x0F0E0D0C0B0A0908,
+                0x0706050403020100,
+            ],
+            dtype=np.uint64,
+        )
+        plaintext = np.array([0x74206E69206D6F6F, 0x6D69732061207369], dtype=np.uint64)
+        expected_ciphertext = np.array(
+            [0x8D2B5579AFC8A3A0, 0x3BF72A87EFE7B868], dtype=np.uint64
+        )
         ciphertext = simon.encrypt_block(algo, plaintext, key)
         np.testing.assert_array_equal(ciphertext, expected_ciphertext)

0c38b1dd2fdeceeed8009185a1278a39d58ed0f9
Makowe <nico.makowe01@gmail.com>
2026-02-25 12:20:07 +0100

    remove unused files

--- debug/simon64_128_masked.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <stdlib.h>
-
-extern uint32_t expandedKey[];
-extern uint32_t masks[];
-
-void simon64_128_seed(uint8_t *seed);
-void simon64_128_set_key(uint8_t *k);
-void simon64_128_encrypt(uint8_t *pt, uint8_t *ct);
